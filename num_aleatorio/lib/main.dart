6faa3be5d56570e5de022a378d93c68f9739afbb
Renan MP <renanmp2015@gmail.com>
2021-10-15 13:58:14 -0300

    App07

--- num_aleatorio/lib/main.dart
+++ num_aleatorio/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -21,10 +23,167 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController guess = new TextEditingController();
+  bool _pressed = false;
+  var change_green = Colors.green;
+  var change_cont = Colors.yellow;
+  var x = null;
+  var im = "assets/images/BackStatic.png";
+
+  void reset() {
+    setState(() {
+      change_green = Colors.green;
+      change_cont = Colors.yellow;
+      _pressed = false;
+      im = "assets/images/BackStatic.png";
+      x = null;
+    });
+  }
+
+  void guess_it() {
+    setState(() {
+      int guessed = int.parse(guess.text);
+      var random = new Random();
+      x = random.nextInt(9) + 1;
+      _pressed = true;
+      change_green = Colors.grey;
+
+      if (guessed == x) {
+        change_cont = Colors.green;
+        im = "assets/images/BackGreen.png";
+      } else {
+        change_cont = Colors.red;
+        im = "assets/images/BackRed.png";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Game")),
+      appBar: title(),
+      body: body(),
+    );
+  }
+
+  title() {
+    return AppBar(
+      title: Text("Descubra o número aleatório",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          )),
+      centerTitle: true,
+      backgroundColor: Colors.purple,
+    );
+  }
+
+  body() {
+    return Container(
+        width: 360,
+        height: 515,
+        decoration: background(im),
+        child: SingleChildScrollView(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              column(),
+            ],
+          ),
+        ));
+  }
+
+  column() {
+    return Padding(
+        padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
+        child: Column(children: <Widget>[
+          container(change_cont),
+          text_container(textfield()),
+          button(change_green),
+        ]));
+  }
+
+  container(change) {
+    return Container(
+      height: 240,
+      width: 240,
+      color: Colors.transparent,
+      child: Center(
+        child: Text(
+          _pressed ? "$x" : "?",
+          style: TextStyle(
+            color: change,
+            fontSize: 50,
+          ),
+        ),
+      ),
+    );
+  }
+
+  text_container(content) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+      height: 80,
+      width: 250,
+      child: content,
+    );
+  }
+
+  textfield() {
+    return TextField(
+      keyboardType: TextInputType.number,
+      controller: guess,
+      decoration: InputDecoration(
+        hintText: "Qual número ela está pensando?",
+        hintStyle: TextStyle(fontSize: 15, color: Colors.black),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+
+  button(change_green) {
+    return Row(children: <Widget>[
+      Container(
+          padding: EdgeInsets.fromLTRB(0, 0, 5, 0),
+          width: 190,
+          height: 50,
+          child: ElevatedButton(
+            onPressed: _pressed ? null : guess_it,
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(change_green),
+            ),
+            child: Text("Adivinhar",
+                style: TextStyle(fontSize: 20, color: Colors.white)),
+          )),
+      Container(
+          padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+          width: 60,
+          height: 50,
+          child: ElevatedButton(
+            onPressed: reset,
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.red),
+            ),
+            child: Icon(
+              Icons.restart_alt,
+              size: 30,
+              color: Colors.white,
+            ),
+          ))
+    ]);
+  }
+
+  background(image_ad) {
+    return BoxDecoration(
+      image: DecorationImage(
+        image: AssetImage(image_ad),
+        fit: BoxFit.cover,
+      ),
     );
   }
 }
